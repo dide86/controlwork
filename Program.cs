a149bfedd29cb606352c9f4ed690c3985a05e411
dide <justgamegeorge@gmail.com>
2022-04-05 19:43:47 +0500

    закончил программу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+string[] massiv = {"Hello", "442", "World", "45", "G"};
+for ( int index = 0;  index< massiv.Length; index++) {
+if (massiv[index].Length <= 3)
+WriteLine(massiv[index]);
+}
