cc6da830da8cb9c2c6934296c3e0d24c8d6626ba
dide <justgamegeorge@gmail.com>
2022-04-09 10:12:15 +0500

    Исправил работу

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 ﻿using static System.Console;
-string[] massiv = {"Hello", "442", "World", "45", "G"};
-for ( int index = 0;  index< massiv.Length; index++) {
-if (massiv[index].Length <= 3)
-WriteLine(massiv[index]);
+string[] massiv = { "Hello", "442", "World", "45", "G", "}{e}{", "RFRF", "r2d2", "rok", "ku" };
+string[] newMassiv = new String[massiv.Length];
+int newIndex = 0;
+for (int index = 0; index < massiv.Length; index++)
+{
+    Write($"{massiv[index]} ");
+    if (massiv[index].Length <= 3)
+    {
+        newMassiv[newIndex] = massiv[index];
+        newIndex++;
+    }
+}
+WriteLine();
+for (int index = 0; index < newMassiv.Length; index++)
+{
+    if (newMassiv[index] != null)
+        Write($"{newMassiv[index]} ");
 }
